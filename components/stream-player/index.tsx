a9998b46f18017c06333e228504d9441e7f3afe7
lunaticenslaved <lunaticenslaved@yandex.ru>
2024-01-04 00:28:16 +0300

    feat(stream): add header component

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -10,6 +10,7 @@ import { useChatSidebar } from '@/store/use-chat-sidebar';
 import { Chat } from './chat';
 import { ChatSkeleton } from './chat-community';
 import { ChatToggle } from './chat-toggle';
+import { Header, HeaderSkeleton } from './header';
 import { Video, VideoSkeleton } from './video';
 
 interface StreamPlayerProps {
@@ -42,6 +43,14 @@ export function StreamPlayer({ user, isFollowing, stream }: StreamPlayerProps) {
         )}>
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user.username} hostIdentity={user.id} />
+          <Header
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            imageUrl={user.imageUrl}
+            isFollowing={isFollowing}
+            name={stream.name}
+          />
         </div>
         <div className={cn('col-span-1', collapsed && 'hidden')}>
           <Chat
@@ -64,6 +73,7 @@ export function StreamPlayerSkeleton() {
     <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
       <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
         <VideoSkeleton />
+        <HeaderSkeleton />
       </div>
       <div className="col-span-1 bg-background">
         <ChatSkeleton />
